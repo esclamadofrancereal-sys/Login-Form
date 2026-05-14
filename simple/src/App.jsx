972119332f4ba2758690e5512ec0a56c1478eb27
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2026-05-15 00:03:34 +0800

    initial

--- simple/src/App.jsx
+++ simple/src/App.jsx
@@ -17,22 +17,19 @@ const App = () => {
 
       <form action="#" className="login-form">
         <div className="input-wrapper">
-          <input type="email" placeholder="Email" required />
+          <input type="email" placeholder="Email" className="input-field" required />
           <img src="/mail.svg" alt="" className="mail-icon" />
         </div>
-      </form>
 
-
-      <form action="#" className="login-form">
         <div className="input-wrapper">
-          <input type="password" placeholder="Password" required />
-          <img src="/lock.svg" alt="" className="lock-icon" />
+          <input type="password" placeholder="Password" className="input-field" required />
+          <img src="/lock.svg" alt="" className="mail-icon" />
         </div>
         <a href="#" className="forgotpassword">Forgot Password?</a>
-        <button type="submit" className="login-btn">Log In</button>
-      </form>
+        <button type="submit" className="Loginbtn">Log In</button>
 
-      <p className="signup">Don't have an account? <a href="#" className="signup-link">Sign Up</a></p>
+        <p className="signup">Don't have an account? <a href="#" className="signup-link">Sign Up</a></p>
+       </form>
     </div>
   )
 }
