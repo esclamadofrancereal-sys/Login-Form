6a5205c2f466c287538f1259ee584f208d583d66
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2026-05-14 21:49:32 +0800

    try

--- simple/src/App.jsx
+++ simple/src/App.jsx
@@ -4,9 +4,23 @@ const App = () => {
       <h2 className="form">Log in with</h2>
       <div className="social-login">
         <button className="social-btn">
-          <img src="" alt="" className="email" />
+          <img src="/google.svg" alt="Google" className="google-icon" />
+          Google
+        </button>
+        <button className="social-btn">
+          <img src="/facebook.svg" alt="Facebook" className="facebook-icon" />
+          Facebook
         </button>
       </div>
+
+      <p className="separator"><span>or</span></p>
+
+      <form action="#" className="login-form">
+        <div className="input-wrapper">
+          <input type="email" placeholder="Email" required />
+
+        </div>
+      </form>
     </div>
   )
 }
