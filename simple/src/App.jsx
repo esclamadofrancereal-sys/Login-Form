30c9931b95ffee9bee16a21946fc212dacfe67e8
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2026-05-17 21:07:48 +0800

    separated the social button

--- simple/src/App.jsx
+++ simple/src/App.jsx
@@ -2,17 +2,7 @@ const App = () => {
   return (
     <div className = "Login-Container">
       <h2 className="form">Log in with</h2>
-      <div className="social-login">
-        <button className="social-btn">
-          <img src="/google.svg" alt="Google" className="google-icon" />
-          Google
-        </button>
-        <button className="social-btn">
-          <img src="/facebook.svg" alt="Facebook" className="facebook-icon" />
-          Facebook
-        </button>
-      </div>
-
+      
       <p className="separator"><span>or</span></p>
 
       <form action="#" className="login-form">
